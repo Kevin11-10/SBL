98caf62bd189c039839f059a095accbc3f49a80e
Kevin Otok <kevinotok8@gmail.com>
2026-07-18 14:31:49 +0100

    Update data.js

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ const TEAMS = [
     { id: 2, name: "Otokolo FC", manager: 'Otokolo' },
     { id: 3, name: "Divine FC", manager: 'Divine' },
     { id: 4, name: "Adedoyin United", manager: 'Adedoyin' },
-    { id: 5, name: "Somto FC", manager: 'Somto' },
-    { id: 6, name: "Emmanuel United", manager: 'Emmanuel' },
-    { id: 7, name: "Layomi FC", manager: 'Layomi' },
+    { id: 5, name: "Real Maadrid", manager: 'Somto' },
+    { id: 6, name: "Obakings", manager: 'Oba' },
+    { id: 7, name: "Liverpool", manager: 'Layomi' },
     { id: 8, name: "Ebenezer FC", manager: 'Ebenezer' }
 ];
 
@@ -21,79 +21,79 @@ function generateFixtures() {
         // Round 1 (Home)
         { home: 'Otokolo', away: 'Divine', round: 1 },
         { home: 'Adedoyin', away: 'Kevin', round: 1 },
-        { home: 'Somto', away: 'Emmanuel', round: 1 },
+        { home: 'Somto', away: ' Oba', round: 1 },
         { home: 'Layomi', away: 'Ebenezer', round: 1 },
         // Round 2 (Away)
         { home: 'Divine', away: 'Otokolo', round: 2 },
         { home: 'Kevin', away: 'Adedoyin', round: 2 },
-        { home: 'Emmanuel', away: 'Somto', round: 2 },
+        { home: ' Oba', away: 'Somto', round: 2 },
         { home: 'Ebenezer', away: 'Layomi', round: 2 },
 
         // Round 3 (Home)
         { home: 'Otokolo', away: 'Kevin', round: 3 },
-        { home: 'Divine', away: 'Emmanuel', round: 3 },
+        { home: 'Divine', away: ' Oba', round: 3 },
         { home: 'Adedoyin', away: 'Ebenezer', round: 3 },
         { home: 'Somto', away: 'Layomi', round: 3 },
         // Round 4 (Away)
         { home: 'Kevin', away: 'Otokolo', round: 4 },
-        { home: 'Emmanuel', away: 'Divine', round: 4 },
+        { home: ' Oba', away: 'Divine', round: 4 },
         { home: 'Ebenezer', away: 'Adedoyin', round: 4 },
         { home: 'Layomi', away: 'Somto', round: 4 },
 
         // Round 5 (Home)
-        { home: 'Otokolo', away: 'Emmanuel', round: 5 },
+        { home: 'Otokolo', away: ' Oba', round: 5 },
         { home: 'Kevin', away: 'Ebenezer', round: 5 },
         { home: 'Divine', away: 'Layomi', round: 5 },
         { home: 'Adedoyin', away: 'Somto', round: 5 },
         // Round 6 (Away)
-        { home: 'Emmanuel', away: 'Otokolo', round: 6 },
+        { home: ' Oba', away: 'Otokolo', round: 6 },
         { home: 'Ebenezer', away: 'Kevin', round: 6 },
         { home: 'Layomi', away: 'Divine', round: 6 },
         { home: 'Somto', away: 'Adedoyin', round: 6 },
 
         // Round 7 (Home)
         { home: 'Otokolo', away: 'Ebenezer', round: 7 },
-        { home: 'Emmanuel', away: 'Layomi', round: 7 },
+        { home: ' Oba', away: 'Layomi', round: 7 },
         { home: 'Kevin', away: 'Somto', round: 7 },
         { home: 'Divine', away: 'Adedoyin', round: 7 },
         // Round 8 (Away)
         { home: 'Ebenezer', away: 'Otokolo', round: 8 },
-        { home: 'Layomi', away: 'Emmanuel', round: 8 },
+        { home: 'Layomi', away: ' Oba', round: 8 },
         { home: 'Somto', away: 'Kevin', round: 8 },
         { home: 'Adedoyin', away: 'Divine', round: 8 },
 
         // Round 9 (Home)
         { home: 'Otokolo', away: 'Layomi', round: 9 },
         { home: 'Ebenezer', away: 'Somto', round: 9 },
-        { home: 'Emmanuel', away: 'Adedoyin', round: 9 },
+        { home: ' Oba', away: 'Adedoyin', round: 9 },
         { home: 'Kevin', away: 'Divine', round: 9 },
         // Round 10 (Away)
         { home: 'Layomi', away: 'Otokolo', round: 10 },
         { home: 'Somto', away: 'Ebenezer', round: 10 },
-        { home: 'Adedoyin', away: 'Emmanuel', round: 10 },
+        { home: 'Adedoyin', away: ' Oba', round: 10 },
         { home: 'Divine', away: 'Kevin', round: 10 },
 
         // Round 11 (Home)
         { home: 'Otokolo', away: 'Somto', round: 11 },
         { home: 'Layomi', away: 'Adedoyin', round: 11 },
         { home: 'Ebenezer', away: 'Divine', round: 11 },
-        { home: 'Emmanuel', away: 'Kevin', round: 11 },
+        { home: ' Oba', away: 'Kevin', round: 11 },
         // Round 12 (Away)
         { home: 'Somto', away: 'Otokolo', round: 12 },
         { home: 'Adedoyin', away: 'Layomi', round: 12 },
         { home: 'Divine', away: 'Ebenezer', round: 12 },
-        { home: 'Kevin', away: 'Emmanuel', round: 12 },
+        { home: 'Kevin', away: ' Oba', round: 12 },
 
         // Round 13 (Home)
         { home: 'Otokolo', away: 'Adedoyin', round: 13 },
         { home: 'Somto', away: 'Divine', round: 13 },
         { home: 'Layomi', away: 'Kevin', round: 13 },
-        { home: 'Ebenezer', away: 'Emmanuel', round: 13 },
+        { home: 'Ebenezer', away: ' Oba', round: 13 },
         // Round 14 (Away)
         { home: 'Adedoyin', away: 'Otokolo', round: 14 },
         { home: 'Divine', away: 'Somto', round: 14 },
         { home: 'Kevin', away: 'Layomi', round: 14 },
-        { home: 'Emmanuel', away: 'Ebenezer', round: 14 }
+        { home: ' Oba', away: 'Ebenezer', round: 14 }
     ];
 
     schedule.forEach(match => {
